180d5d87308a295c97aa68e6ccdbfa243eca68ce
brahan <brahanleandro@gmail.com>
2020-11-08 11:56:08 -0500

    i'm starting the project

---
+++ script.js
@@ -0,0 +1,31 @@
+const quizData = [
+	{
+		question : 'How old is Florin?',
+		a: '10',
+		b: '17',
+		c: '26',
+		d: '110',
+		correct: 'c'
+	}, {
+		question: 'Who is he most used programming languege in 2020?',
+		a: 'JavaScript',
+		b: 'Java',
+		c: 'C++',
+		d: 'Fortran',
+		correct: 'a'
+	}, {
+		question: 'Who won the presidency of the united states 2020?'
+		a: 'Donald Trump',
+		b: 'Joe Biden',
+		c: 'Howie Hawkins',
+		d: 'Jo Jorgensen',
+		correct: 'b'
+	}, {
+		question: 'What does HTML stand for'
+		a: '',
+		b: '',
+		c: '',
+		d: '',
+		correct: ''
+	}
+]
